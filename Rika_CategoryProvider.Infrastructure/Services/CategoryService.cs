d026578e3e012bc11360c6d7748c95f5e6e1999e
Hanna Grehn <hannagrehn@gmail.com>
2024-10-31 09:33:53 +0100

    service done

--- Rika_CategoryProvider.Infrastructure/Services/CategoryService.cs
+++ Rika_CategoryProvider.Infrastructure/Services/CategoryService.cs
@@ -1,47 +1,51 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Rika_CategoryProvider.Infrastructure.Context;
-//using Rika_CategoryProvider.Infrastructure.Entities;
-//using Rika_CategoryProvider.Infrastructure.Repos;
+﻿using Microsoft.Extensions.Logging;
+using Rika_CategoryProvider.Infrastructure.Entities;
+using Rika_CategoryProvider.Infrastructure.Repos;
 
-//public class CategoryService(CategoryRepository categoryRepository)
-//{
-//	private readonly CategoryRepository _categoryRepository = categoryRepository;
+public class CategoryService
+{
+	private readonly CategoryRepository _categoryRepository;
+	private readonly ILogger<CategoryService> _logger;
 
-//	public async Task<CategoryEntity> AddCategoryAsync(string categoryName)
-//	{
-//		var result = await _categoryRepository.GetCategoryAsync(x => x.CategoryName = categoryName);
+	public CategoryService(CategoryRepository categoryRepository, ILogger<CategoryService> logger)
+	{
+		_categoryRepository = categoryRepository;
+		_logger = logger;
+	}
+
+	public async Task<CategoryEntity> AddCategoryAsync(string categoryName)
+	{
+		var existingCategory = await _categoryRepository.GetCategoryAsync(x => x.CategoryName == categoryName);
+		if (existingCategory != null)
+		{
+			throw new InvalidOperationException("Category already exists.");
+		}
+
+		var newCategory = new CategoryEntity
+		{
+			CategoryName = categoryName
+		};
+
+		var addedCategory = await _categoryRepository.AddAsync(newCategory);
+		return addedCategory;
+	}
+
+	public async Task<IEnumerable<CategoryEntity>> GetAllCategoriesAsync()
+	{
+		try
+		{
+			return await _categoryRepository.GetAllAsync();
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(ex, "An error occurred while getting all categories.");
+			throw; 
+		}
+	}
+}
 
 
-//		await _context.Categories.AddAsync(entity);
-//		await _context.SaveChangesAsync();
-//		return entity;
-//	}
 
-//	public async Task<CategoryEntity> UpdateCategoryAsync(CategoryEntity entity)
-//	{
-//		_context.Categories.Update(entity);
-//		await _context.SaveChangesAsync();
-//		return entity;
-//	}
-
-//	public async Task<CategoryEntity> DeleteCategoryAsync(CategoryEntity entity)
-//	{
-//		_context.Categories.Remove(entity);
-//		await _context.SaveChangesAsync();
-//		return entity;
-//	}
-
-//	public async Task<CategoryEntity?> GetCategoryByIdAsync(int id)
-//	{
-//		return await _context.Categories.FindAsync(id);
-//	}
-
-//	public async Task<IEnumerable<CategoryEntity>> GetAllCategoriesAsync()
-//	{
-//		return await _context.Categories.ToListAsync();
-//	}
-
-//}
 
 
 
--- Rika_CategoryProvider/Program.cs
+++ Rika_CategoryProvider/Program.cs
@@ -4,8 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Rika_CategoryProvider.Infrastructure.Context;
-
-
+using Rika_CategoryProvider.Infrastructure.Repos;
 
 var host = new HostBuilder()
 	.ConfigureFunctionsWebApplication()
@@ -14,12 +13,14 @@ var host = new HostBuilder()
 		services.AddApplicationInsightsTelemetryWorkerService();
 		services.ConfigureFunctionsApplicationInsights();
 
-		services.AddDbContext<CategoryDbContext>(options => options.UseSqlServer(context.Configuration.GetConnectionString("OrderProviderDb")));
-	
+		services.AddDbContext<CategoryDbContext>(options =>
+			options.UseSqlServer(context.Configuration.GetConnectionString("OrderProviderDb")));
 
+		services.AddScoped<CategoryRepository>();
+		services.AddScoped<CategoryService>();
+
+		services.AddLogging();
 	})
 	.Build();
-
-
 
 host.Run();
