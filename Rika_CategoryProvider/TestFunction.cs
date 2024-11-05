89ba7a442f0185767e9ff80f1bd94e9c02223fc1
Hanna Grehn <hannagrehn@gmail.com>
2024-11-05 16:19:48 +0100

    try different approach

--- Rika_CategoryProvider/TestFunction.cs
+++ Rika_CategoryProvider/TestFunction.cs
@@ -5,20 +5,20 @@ using Microsoft.Extensions.Logging;
 
 namespace Rika_CategoryProvider
 {
-    public class TestFunction
-    {
-        private readonly ILogger<TestFunction> _logger;
+	public class TestFunction
+	{
+		private readonly ILogger<TestFunction> _logger;
 
-        public TestFunction(ILogger<TestFunction> logger)
-        {
-            _logger = logger;
-        }
+		public TestFunction(ILogger<TestFunction> logger)
+		{
+			_logger = logger;
+		}
 
-        [Function("TestFunction")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
-    }
+		[Function("TestFunction")]
+		public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+		{
+			_logger.LogInformation("C# HTTP trigger function processed a request.");
+			return new OkObjectResult("Welcome to Azure Functions!");
+		}
+	}
 }
